df8add4657931d12444b46d890660b4b0ecc3b2d
Kyle Zheng <kylezheng73@gmail.com>
2023-07-29 21:10:08 -0400

    copy files + format

---
+++ packages/plugin-core/src/plugin.js
@@ -0,0 +1,25 @@
+const Core = require("./core");
+const evaluateFunctionParameters = require("./util").evaluateFunctionParameters;
+
+jsPsych.plugins["pcllab-core"] = (function () {
+  let plugin = {};
+
+  plugin.info = {
+    name: "pcllab-core",
+    parameters: {},
+  };
+
+  plugin.trial = function (display_element, trial) {
+    trial = evaluateFunctionParameters(trial);
+
+    $("body").bind("copy paste", function (e) {
+      e.preventDefault();
+      return false;
+    });
+
+    const core = new Core(display_element, trial);
+    core.start();
+  };
+
+  return plugin;
+})();
